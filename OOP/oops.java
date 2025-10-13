5db145717f116b921c3e64fda7510d6ff14031a3
Lokesh <lkhairnar580@gmail.com>
2025-10-13 18:50:22 +0530

    oop 1st concept

--- OOP/oops.java
+++ OOP/oops.java
@@ -1,32 +1,23 @@
-public class oops{
+public class  oops{
     public static void main(String[] args) {
-        Pen p1=new Pen();//created a pen object named p1
-        p1.setcolor("blue");
-        System.out.println(p1.getcolor());
-        p1.settip(5);
-        System.out.println(p1.gettip());
-       
-        
-
+        Pen p1=new Pen();//create the pen obj p1
+        p1.setcolor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(10);
+        System.out.println(p1.tip);
     }
 }
-
 class Pen{
-    //prop+fun
+    //properties+function
+
     String color;
     int tip;
-    String getcolor(){
-        return this.color;
-    }
-    int gettip(){
-        return  this.tip;
+
+    void setcolor(String newColor){
+        color=newColor;
     }
 
-    void setcolor(String newcolor){
-        this.color=newcolor;
-    }
-
-    void settip(int tip){
-        this.tip=tip;
+    void setTip(int newTip){
+        tip=newTip;
     }
 }
